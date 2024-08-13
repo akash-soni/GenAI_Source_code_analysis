a7eda138cf3389225967c23ddd0ab7d6991edc37
akash-soni <akash.200287@gmail.com>
2024-08-13 10:32:27 +0530

    changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'akash soni',
+    author_email= 'akashsoni.in@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
